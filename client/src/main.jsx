07cef93d460d859de2e1c2b299bf514cb36864fb
Reason Shrestha <sthareason264@gmail.com>
2024-02-21 09:53:58 +0545

    Client routing setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import { Home, Orders, Customers } from './pages'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/customers',
+        element: <Customers />
+      },
+      {
+        path: '/orders',
+        element: <Orders />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
